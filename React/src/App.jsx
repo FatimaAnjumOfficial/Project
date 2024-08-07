391622e0240c4fc7895a6781ff05675bc9b1595c
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-07 15:00:53 +0500

    Create App.jsx

---
+++ React/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Header />
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;

048d5b05d32115d142b146f4a6cb20836a8314d6
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-08 01:01:20 +0500

    Update App.jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+import HomePage from "./components/pages/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,8 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <Header />
-        <Navbar />
+        <HomePage />
       </div>
     </>
   );

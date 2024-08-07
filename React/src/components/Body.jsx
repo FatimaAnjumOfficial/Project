ebf0d7b9354610c53cd04b3cba2a5c4f859f0c10
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-08 00:34:04 +0500

    Add files via upload

---
+++ React/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import img from "./HomePage.webp";
+import "./Body.css";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <img src={img} alt="HomePage Body" />
+    </div>
+  );
+};
+
+export default Body;

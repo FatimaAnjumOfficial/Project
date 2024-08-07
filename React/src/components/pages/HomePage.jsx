c4e454ee09a9b7f0449061547db46dc3525cd882
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-08 01:04:19 +0500

    Create HomePage.jsx

---
+++ React/src/components/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header";
+import Navbar from "../Navbar";
+import Body from "../Body";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Body />
+    </div>
+  );
+};
+
+export default HomePage;

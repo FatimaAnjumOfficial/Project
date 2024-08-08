9f76e8d29bbeab264f33c21f8388cb44b5e81c4a
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-09 02:28:15 +0500

    Update Register.jsx

--- React/Travelling Website/src/components/shared/Register.jsx
+++ React/Travelling Website/src/components/shared/Register.jsx
@@ -1,249 +1,135 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./StylishButton.css";
-import "./HomePage.css";
+import avatar from "../assets/avatar.png";
+import logo from "../components/images/Musaafir.png";
+import {
+  Bag,
+  BarChart,
+  ChevronDown,
+  Cursor,
+  Menu,
+  Plus,
+  DownArrow,
+} from "../assets/Icons";
+import { Button } from "../components/shared/Button";
+import { LayoutContainer } from "./LayoutContainer";
+import "./Logo.css";
+import Register from "../components/Register";
+import "../components/StylishButton.css";
+import { BiBorderRadius } from "react-icons/bi";
 
-function Register() {
-  const [showModal, setShowModal] = useState(false);
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phoneNo: "",
-    age: "",
-    gender: "",
-    address: "",
-    country: "",
-    city: "",
-  });
-  const [error, setError] = useState("");
-  const [imagePreview, setImagePreview] = useState("");
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleFileChange = async (e) => {
-    const file = e.target.files[0];
-
-    if (file) {
-      const formData = new FormData();
-      formData.append("image", file);
-
-      try {
-        await axios.post("http://localhost:4000/nfts", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
-        console.log("Image File uploaded successfully");
-
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          setImagePreview(reader.result); // Set the preview URL
-        };
-        reader.readAsDataURL(file);
-      } catch (error) {
-        setError("Error uploading image file.");
-      }
-    }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post("http://localhost:4000/nfts", formData);
-
-      setFormData({
-        firstName: "",
-        lastName: "",
-        email: "",
-        phoneNo: "",
-        age: "",
-        gender: "",
-        address: "",
-        country: "",
-        city: "",
-      });
-      setShowModal(false);
-    } catch (error) {
-      setError("Failed to submit registration.");
-    }
-  };
-
+export function Navbar() {
   return (
-    <>
-      <div>
-        <div style={{ display: "flex", gap: "7px", marginLeft: "175px" }}>
-          <button onClick={() => setShowModal(true)} className="stylish-button">
-            Register
-          </button>
-        </div>
-        {showModal && (
-          <div className="modal">
-            <div className="modal-content">
-              <span className="close" onClick={() => setShowModal(false)}>
-                &times;
-              </span>
-              <h2 style={{ textAlign: "center", fontSize: "25px", top: "0" }}>
-                Registration Form
-              </h2>
-              {error && <p className="error">{error}</p>}
-              <form onSubmit={handleSubmit}>
-                <label>
-                  First Name:
-                  <input
-                    type="text"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-                <label>
-                  Last Name:
-                  <input
-                    type="text"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-                <label>
-                  Email:
-                  <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-                <label>
-                  Phone No:
-                  <input
-                    type="text"
-                    name="phoneNo"
-                    value={formData.phoneNo}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-                <label>
-                  Age:
-                  <input
-                    type="number"
-                    name="age"
-                    value={formData.age}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-                <div style={{ display: "flex", gap: "10px" }}>
-                  <label>
-                    Gender:{" "}
-                    <select
-                      name="gender"
-                      value={formData.gender}
-                      onChange={handleInputChange}
-                      required
-                      style={{
-                        border: "1px solid grey",
-                        borderRadius: "10px",
-                        padding: "4px 6px",
-                        textAlign: "center",
-                      }}
-                    >
-                      <option value="" style={{ fontWeight: "bold" }}>
-                        Select Gender
-                      </option>
-                      <option value="male">Male</option>
-                      <option value="female">Female</option>
-                      <option value="other">Other</option>
-                    </select>
-                  </label>
-                  <label>
-                    <span style={{ color: "white" }}>_________</span>
-                    Country:{" "}
-                    <select
-                      name="country"
-                      value={formData.country}
-                      onChange={handleInputChange}
-                      required
-                      style={{
-                        border: "1px solid grey",
-                        borderRadius: "10px",
-                        padding: "4px 6px",
-                        textAlign: "center",
-                      }}
-                    >
-                      <option value="" style={{ fontWeight: "bold" }}>
-                        Select Country
-                      </option>
-                      <option value="Pakistan">Pakistan</option>
-                      <option value="China">China</option>
-                      <option value="Japan">Japan</option>
-                      <option value="Other">Other</option>
-                    </select>
-                  </label>
-                  <label>
-                    <span style={{ color: "white" }}>_________</span>City:{" "}
-                    <select
-                      name="city"
-                      value={formData.city}
-                      onChange={handleInputChange}
-                      required
-                      style={{
-                        border: "1px solid grey",
-                        borderRadius: "10px",
-                        padding: "4px 6px",
-                        textAlign: "center",
-                      }}
-                    >
-                      <option value="" style={{ fontWeight: "bold" }}>
-                        Select City
-                      </option>
-                      <option value="Lahore">Lahore</option>
-                      <option value="Karachi">Karachi</option>
-                      <option value="Other">Other</option>
-                    </select>
-                  </label>
-                </div>
-                <label>
-                  Address:
-                  <input
-                    type="text"
-                    name="address"
-                    value={formData.address}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </label>
-
-                {imagePreview && (
-                  <div className="image-preview-container">
-                    <img
-                      src={imagePreview}
-                      alt="Preview"
-                      className="image-preview"
-                    />
-                  </div>
-                )}
-                <button type="submit" className="submit-button">
-                  Submit
-                </button>
-              </form>
+    <LayoutContainer>
+      <div className="p-4 md:hidden">
+        <Menu />
+      </div>
+      <div
+        className="justify-between hidden md:flex"
+        style={{
+          display: "flex",
+          background: "linear-gradient(to right, #000000, #000000ff, #49032e)",
+          border: "1px solid black",
+          padding: "0px 40px",
+        }}
+      >
+        <div className="flex gap-8 items-center">
+          <div className="logo">
+            <div
+              style={{
+                flexDirection: "column",
+                fontFamily: "cursive",
+              }}
+            >
+              <div className="logo-container">
+                <img src={logo} alt="Logo" />
+                <h5>Where Luxurious Meets Stay</h5>
+              </div>
             </div>
           </div>
-        )}
+          <div
+            style={{
+              fontFamily: "Times New Roman, serif",
+              fontSize: "18px",
+              listStyle: "none",
+              margin: 0,
+              padding: 0,
+              justifyContent: "center",
+            }}
+          >
+            <ul className="flex gap-8 items-center">
+              <NavItem title="Home" url="#" />
+              <NavItem
+                title="Hotels"
+                url="/hotels"
+                style={{ borderRadius: "20px" }}
+                submenu={[
+                  { title: "Luxury Hotels", url: "/hotels/luxury-hotels" },
+                  { title: "Budget Hotels", url: "/hotels/" },
+                  { title: "Hotel Deals", url: "/hotels/hotel-deals" },
+                ]}
+              />
+              <NavItem
+                title="Flights"
+                url="/flights"
+                submenu={[
+                  { title: "Domestic Flights", url: "#" },
+                  { title: "International Flights", url: "#" },
+                  { title: "Flight Deals", url: "#" },
+                ]}
+              />
+              <NavItem title="Packages" url="/packages" />
+              <NavItem title="About Us" url="/about-us" />
+              <NavItem title="Contact Us" url="/contact-us" />
+            </ul>
+          </div>
+        </div>
+        <div className="flex gap-4 py-11">
+          <Register />
+
+          {/* <Button
+            className="flex items-center gap-1 rounded-xl px-3 bg-zinc-700"
+            variant="ghost"
+          >
+            <span>Network</span>
+            <Plus />
+          </Button>
+          <div className="flex gap-1 items-center">
+              <img className="h-6 w-6" src={avatar} />
+            <Button variant="ghost" size="icon">
+              <ChevronDown />
+            </Button>
+          </div>*/}
+        </div>
       </div>
-    </>
+    </LayoutContainer>
   );
 }
 
-export default Register;
+function NavItem({ Icon, title, url, isActive = false, submenu = [] }) {
+  return (
+    <li className="relative group">
+      <a
+        className={`flex items-center gap-2 px-3 py-2 rounded-xl transition-colors duration-300 ${
+          isActive
+            ? "bg-gray-700 text-white"
+            : "text-gray-300 hover:bg-gray-600 hover:text-white"
+        }`}
+        href={url}
+      >
+        {Icon && <Icon />}
+        <span>{title}</span>
+        {submenu.length > 0 && <ChevronDown className="ml-2" />}
+      </a>
+      {submenu.length > 0 && (
+        <ul className="absolute left-0 top-full mt-2 bg-white text-black shadow-lg group-hover:block hidden w-48">
+          {submenu.map((item, index) => (
+            <li key={index} className="border-b last:border-b-0">
+              <a className="block px-4 py-2 hover:bg-gray-200" href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+}

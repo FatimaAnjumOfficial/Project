4d827858068ed215c4e2aba93662874bc51283e9
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-09 00:50:07 +0500

    Update MarketPlaceHeader.jsx

--- React/Travelling Website/src/components/marketPlace/MarketPlaceHeader.jsx
+++ React/Travelling Website/src/components/marketPlace/MarketPlaceHeader.jsx
@@ -1,16 +1,23 @@
-import { Button } from '../shared/Button';
+import { Button } from "../shared/Button";
 
 export function MarketPlaceHeader() {
   return (
-    <div className='flex sm:justify-between sm:flex-row flex-col items-center md:gap-0 gap-5'>
-      <h1 className='text-lg md:text-[28px] font-semibold'>Marketplace</h1>
-      <div className='rounded-full flex items-center px-3 bg-zinc-800 text-xs w-[270px]'>
-        <Button variant='ghost'>Active</Button>
-        <Button className='rounded-full px-3 bg-zinc-700' variant='ghost'>
-          Recommended
-        </Button>
-        <Button variant='ghost'>All Products</Button>
+    <>
+      <br />
+      <div
+        className="flex sm:justify-between sm:flex-row flex-col items-center md:gap-0 gap-5"
+        style={{ padding: "0px 40px" }}
+      >
+        <h1 className="text-lg md:text-[28px] font-semibold">Marketplace</h1>
+        <div className="rounded-full flex items-center px-3 bg-zinc-800 text-xs w-[270px]">
+          <Button variant="ghost">Active</Button>
+          <Button className="rounded-full px-3 bg-zinc-700" variant="ghost">
+            Recommended
+          </Button>
+          <Button variant="ghost">All Products</Button>
+        </div>
       </div>
-    </div>
+      <br />
+    </>
   );
 }

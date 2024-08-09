1afc6b68dbd9e890f5a316d500821f0f88050d74
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-09 22:34:16 +0500

    Update HomePage.jsx

--- React/Travelling Website/src/pages/HomePage.jsx
+++ React/Travelling Website/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
-import { NftSection } from '../components/marketPlace/NftSection';
-import { MarketPlaceFilters } from '../components/marketPlace/MarketPlaceFilters';
-import { MarketPlaceHeader } from '../components/marketPlace/MarketPlaceHeader';
-import { MarketPlaceSubmenu } from '../components/marketPlace/MarketPlaceSubmenu';
-import { LayoutContainer } from '../layout/LayoutContainer';
+import { NftSection } from "../components/marketPlace/NftSection";
+import { MarketPlaceFilters } from "../components/marketPlace/MarketPlaceFilters";
+import { MarketPlaceHeader } from "../components/marketPlace/MarketPlaceHeader";
+import { MarketPlaceSubmenu } from "../components/marketPlace/MarketPlaceSubmenu";
+import { LayoutContainer } from "../layout/LayoutContainer";
+import Body from "../layout/Body";
 
 export function HomePage() {
   return (
     <LayoutContainer>
+      <Body />
       <MarketPlaceHeader />
       <MarketPlaceSubmenu />
       <MarketPlaceFilters />

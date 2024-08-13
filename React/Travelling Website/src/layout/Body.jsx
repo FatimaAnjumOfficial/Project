de00df9f6886eac83ea13b28168014d0b5c0b28e
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-14 02:21:08 +0500

    Update Body.jsx

--- React/Travelling Website/src/layout/Body.jsx
+++ React/Travelling Website/src/layout/Body.jsx
@@ -197,6 +197,126 @@ function Body() {
       <h2 style={{ fontSize: "22px", color: "#7f8c8d", marginBottom: "30px" }}>
         Keep calm & travel on
       </h2>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "30px",
+          flexWrap: "wrap", // Wrap images if they overflow
+          marginTop: "40px",
+          marginBottom: "130px",
+        }}
+      >
+        <div style={{ flexDirection: "column" }}>
+          <div
+            style={{
+              width: "310px",
+              height: "390px",
+              overflow: "hidden",
+              borderRadius: "20px",
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+              transition: "transform 0.3s ease",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src="https://finestayslovenia.com/wp-content/uploads/2020/03/fine-stay-apartments-spring-snow-21.jpg"
+              alt="Camp"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                transition: "transform 0.3s ease",
+              }}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "scale(1.1)")
+              }
+              onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+            />
+          </div>
+          <div style={{ marginTop: "20px" }}>
+            <h2 style={{ fontSize: "15px" }}>
+              <b>Enjoy the great cold</b>
+            </h2>
+            <p style={{ color: "grey", fontSize: "14px" }}>6,986 Properties</p>
+          </div>
+        </div>
+        <div>
+          <div
+            style={{
+              width: "310px",
+              height: "390px",
+              overflow: "hidden",
+              borderRadius: "20px",
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+              transition: "transform 0.3s ease",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src="https://media.istockphoto.com/id/1319606736/photo/idyllic-country-house-at-sunrise.jpg?s=612x612&w=0&k=20&c=nVKccrQ24p0PE31iNK4Nko24VZOGMHxpKm763LAiKOw="
+              alt="Camp"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                transition: "transform 0.3s ease",
+              }}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "scale(1.1)")
+              }
+              onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+            />
+          </div>
+          <div style={{ marginTop: "20px" }}>
+            <h2 style={{ fontSize: "15px" }}>
+              <b>Pick up the earliest sunrise</b>
+            </h2>
+            <p style={{ color: "grey", fontSize: "14px" }}>3,834 Properties</p>
+          </div>
+        </div>
+        <div style={{ flexDirection: "column" }}>
+          <div
+            style={{
+              width: "310px",
+              height: "390px",
+              overflow: "hidden",
+              borderRadius: "20px",
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+              transition: "transform 0.3s ease",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src="https://content.tui.co.uk/adamtui/2023_8/2_13/6f7a2308-271d-4375-a301-b05200e123d1/ACC_003672_CYP_45WebOriginalCompressed.jpg?i10c=img.resize(width:1080);img.crop(width:1080%2Cheight:608)"
+              alt="Camp"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                transition: "transform 0.3s ease",
+              }}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "scale(1.1)")
+              }
+              onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+            />
+          </div>
+          <div style={{ marginTop: "20px" }}>
+            <h2 style={{ fontSize: "15px" }}>
+              <b>Unique & Luxurious Stay</b>
+            </h2>
+            <p style={{ color: "grey", fontSize: "14px" }}>5,727 Properties</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

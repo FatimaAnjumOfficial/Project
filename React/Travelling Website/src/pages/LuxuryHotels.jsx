db7e8013a9c45068ff33688f58e8075e82f0ea8e
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-14 19:55:30 +0500

    Update LuxuryHotels.jsx

--- React/Travelling Website/src/pages/LuxuryHotels.jsx
+++ React/Travelling Website/src/pages/LuxuryHotels.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+// src/pages/LuxuryHotels.js
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import HotelCard from "../components/marketPlace/HotelCard";
+import "leaflet/dist/leaflet.css";
 import "./LuxuryHotels.css";
+import { hotels } from "../data/hotels";
 
 const LuxuryHotels = () => {
+  const [selectedHotel, setSelectedHotel] = useState(null);
+
+  const handleCardClick = (hotel) => {
+    setSelectedHotel(hotel);
+  };
+
   return (
     <div className="luxury-hotels-container">
       <header className="page-header">
@@ -10,13 +21,58 @@ const LuxuryHotels = () => {
       </header>
       <main className="content">
         <section className="hotel-list">
-          {/* Add your hotel cards or list here */}
-          <div className="hotel-card">
-            <img src="hotel-image-url" alt="Hotel Name" />
-            <h2>Hotel Name</h2>
-            <p>Hotel description goes here.</p>
-          </div>
-          {/* Repeat hotel cards as needed */}
+          {hotels.map((hotel) => (
+            <HotelCard
+              key={hotel.id}
+              image={hotel.image}
+              name={hotel.name}
+              rooms={hotel.rooms}
+              rent={hotel.rent}
+              country={hotel.country}
+              city={hotel.city}
+              onClick={() => handleCardClick(hotel)}
+            />
+          ))}
+        </section>
+
+        {selectedHotel && (
+          <section className="preview">
+            <h2>{selectedHotel.name}</h2>
+            <img src={selectedHotel.image} alt={selectedHotel.name} />
+            <p>
+              <b>Rooms:</b> {selectedHotel.rooms}
+            </p>
+            <p>
+              <b>Rent:</b> {selectedHotel.rent}
+            </p>
+            <p>
+              <b>Country:</b> {selectedHotel.country}
+            </p>
+            <p>
+              <b>City:</b> {selectedHotel.city}
+            </p>
+          </section>
+        )}
+        <section className="map-container">
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            style={{ height: "500px", width: "100%" }}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {hotels.map((hotel) => (
+              <Marker key={hotel.id} position={hotel.position}>
+                <Popup>
+                  {hotel.name}
+                  <br />
+                  {hotel.location}
+                </Popup>
+              </Marker>
+            ))}
+          </MapContainer>
         </section>
       </main>
     </div>

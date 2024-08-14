a102d1554a559bf3a9e1181bc80506e6f3955092
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-14 19:58:45 +0500

    Add files via upload

---
+++ React/Travelling Website/src/components/marketPlace/HotelCard.jsx
@@ -0,0 +1,36 @@
+// src/components/marketPlace/HotelCard.js
+import React from "react";
+import "./HotelCard.css";
+
+const HotelCard = ({ image, name, country, city, rooms, rent, onClick }) => {
+  return (
+    <div className="hotel-card" onClick={onClick}>
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <div style={{ textAlign: "left", justifyContent: "center" }}>
+        <div style={{ display: "flex", gap: "150px" }}>
+          <p>
+            <b>Rooms: </b>
+            {rooms}
+          </p>
+          <p>
+            <b>Rent: </b>
+            {rent}
+          </p>
+        </div>
+        <div style={{ display: "flex", gap: "140px" }}>
+          <p>
+            <b>Country: </b>
+            {country}
+          </p>
+          <p>
+            <b>City: </b>
+            {city}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HotelCard;

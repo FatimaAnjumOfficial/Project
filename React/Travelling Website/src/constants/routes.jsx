1c17f1faa231b053a307f9191036d51e3209829b
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-14 17:17:22 +0500

    Update routes.jsx

--- React/Travelling Website/src/constants/routes.jsx
+++ React/Travelling Website/src/constants/routes.jsx
@@ -1,14 +1,19 @@
-import { HomePage } from '../pages/HomePage';
-import { Layout } from '../layout/Layout';
+import { HomePage } from "../pages/HomePage";
+import { Layout } from "../layout/Layout";
+import LuxuryHotels from "../pages/LuxuryHotels";
 
 export const routes = [
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <HomePage />,
+      },
+      {
+        path: "/hotels/luxury-hotels",
+        element: <LuxuryHotels />,
       },
     ],
   },

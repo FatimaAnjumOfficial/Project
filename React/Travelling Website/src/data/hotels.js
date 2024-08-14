26ba77d33055037b67ba8f14c8130ef2b2f8259d
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-15 01:46:16 +0500

    Create hotels.js

---
+++ React/Travelling Website/src/data/hotels.js
@@ -0,0 +1,118 @@
+export const hotels = [
+  {
+    id: 1,
+    image:
+      "https://www.factmagazines.com/wp-content/uploads/2023/08/W-Al-Marjan-Island-scaled.jpeg",
+    name: "Mandarin Oriental",
+    description:
+      "Mandarin Oriental offers elegant accommodations with stunning city or waterfront views. Guests enjoy world-class dining, a serene spa, and personalized attention in a sophisticated setting.",
+    country: "USA",
+    city: "New York",
+    rooms: "973",
+    rent: "$100,523",
+    interior:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/0f/f6/ee/pool.jpg?w=700&h=-1&s=1",
+    bedroom:
+      "https://www.oppeinhome.com/upload/image/product/thumb/20220810/Modern-UV-Lacquer-Fitted-Wardrobes-Blues-1-OP.webp",
+    position: [51.515, -0.1],
+  },
+  {
+    id: 2,
+    image: "https://thefussyflyer.com/zart/images/662/662216a.webp",
+    name: "Aria Elegance Resort",
+    description:
+      "Discover a haven of sophistication at Aria Elegance Resort, where contemporary design meets serene landscapes. Indulge in luxurious rooms, exquisite dining, and unparalleled relaxation in a stunning environment.",
+    country: "France",
+    city: "Paris",
+    rooms: "364",
+    rent: "$80,673",
+    interior:
+      "https://hard-rock-hotel-dalian.booked.net/data/Photos/OriginalPhoto/10201/1020140/1020140119/Hard-Rock-Hotel-Dalian-Exterior.JPEG",
+    bedroom:
+      "https://img.freepik.com/premium-vector/luxury-bedroom-light-colors-with-golden-furniture-details-big-comfortable-double-royal-bed_858664-3777.jpg",
+    position: [51.515, -0.1],
+  },
+  {
+    id: 3,
+    image:
+      "https://media.istockphoto.com/photos/luxury-resort-picture-id104731717?k=20&m=104731717&s=612x612&w=0&h=40INtJRzhmU1O4Rj24zdY8vj4aGsWpPaEfojaVQ8xBo=",
+    name: "Four Seasons Hotel",
+    description:
+      "Immerse yourself in luxury at Four Seasons Hotel, where elegant accommodations await. Enjoy world-class amenities and personalized experiences in a sophisticated and inviting setting.",
+    country: "USA",
+    city: "New York",
+    rooms: "744",
+    rent: "$90,566",
+    interior:
+      "https://nativainteriors.com/wp-content/uploads/2021/02/Untitled-design-2021-02-19T140117.002-1080x584.png",
+    bedroom:
+      "https://t3.ftcdn.net/jpg/08/33/36/74/360_F_833367440_CQka7gNmjKnlw1USZ2ZwUpedFs12b6mN.jpg",
+    position: [51.515, -0.1],
+  },
+  {
+    id: 4,
+    image:
+      "https://cdn.luxist.org/wp-content/uploads/2023/07/Top-5-Best-Luxury-Hotels-In-Chicago-1.jpg",
+    name: "Hotel de Crillon",
+    description:
+      " Experience timeless elegance at Hotel de Crillon, where historic grandeur meets modern luxury. Nestled in the heart of Paris, this iconic hotel offers exquisite rooms, refined dining, and unparalleled service.",
+    country: "France",
+    city: "Paris",
+    rooms: "534",
+    rent: "$50,673",
+    interior: "https://s.inyourpocket.com/gallery/66578.jpg",
+    bedroom:
+      "https://5.imimg.com/data5/SELLER/Default/2022/5/HV/ZZ/LI/106321217/luxury-bedroom-interior-design-service-500x500.jpg",
+    position: [51.515, -0.1],
+  },
+  {
+    id: 5,
+    image:
+      "https://www.factmagazines.com/wp-content/uploads/2023/09/Nikki-Beach-RAK.jpeg",
+    name: "Marinas Motel",
+    description:
+      "Marinas Motel offers breathtaking ocean views and a relaxing atmosphere. Enjoy comfortable, well-appointed rooms and convenient access to nearby beaches and local attractions. Perfect for a tranquil getaway or a beachside retreat.",
+    country: "UAE",
+    city: "Dubai",
+    rooms: "289",
+    rent: "$40,983",
+    interior:
+      "https://media.istockphoto.com/id/1331465591/photo/3d-render-of-a-luxury-hotel-swimming-pool.jpg?s=612x612&w=0&k=20&c=oK3vEzPg3mZrCXgairNgU5qM-vf0jMab9N7udzzVDk0=",
+    bedroom:
+      "https://beddley.com/cdn/shop/articles/luxury-bedrooms-ideas-exploring-opulence-and-tranquility-beddley-com.jpg?v=1713044454",
+    position: [51.505, -0.09],
+  },
+  {
+    id: 6,
+    image:
+      "https://www.luxurytravelmagazine.com/files/593/4/79684/The_Outpost_Hotel_Pool_Deck_S_bu.jpg",
+    name: "Burj Al Arab Jumeirah",
+    description:
+      "Renowned for its opulence and iconic sail-shaped silhouette, Burj Al Arab Jumeirah offers unparalleled luxury and breathtaking views. Enjoy world-class service and sumptuous accommodations in one of Dubai's most exclusive landmarks.",
+    country: "UAE",
+    city: "Dubai",
+    rooms: "763",
+    rent: "$64,246",
+    interior:
+      "https://the-st-regis-moscow-nikolskaya-hotel.booked.net/data/Photos/OriginalPhoto/2463/246323/246323693/The-St-Regis-Moscow-Nikolskaya-Hotel-Restaurant.JPEG",
+    bedroom:
+      "https://cdn.shopify.com/s/files/1/1482/2692/files/luxury-bedroom.jpg?v=1690474193",
+    position: [51.505, -0.09],
+  },
+  {
+    id: 7,
+    image: "https://thumbs.dreamstime.com/b/luxury-hotel-4480742.jpg",
+    name: "Aman Tokyo",
+    description:
+      "Discover tranquility and modern elegance at Aman Tokyo, where sleek design and serene views offer a peaceful retreat in the heart of the bustling city. ",
+    country: "Japan",
+    city: "Tokyo",
+    rooms: "304",
+    rent: "$56,973",
+    interior:
+      "https://media.architecturaldigest.com/photos/609d676464ce724873e4d1e3/master/w_1600%2Cc_limit/GettyImages-1152668.jpg",
+    bedroom:
+      "https://www.bedsland.co.uk/wp-content/uploads/2023/10/image7-64.webp",
+    position: [51.505, -0.09],
+  },
+];

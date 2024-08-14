84360d0b414229ddc78b68711219ef2d3f752500
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-15 02:02:36 +0500

    Create ContactUs.jsx

---
+++ React/Travelling Website/src/pages/ContactUs.jsx
@@ -0,0 +1,89 @@
+// src/pages/ContactUs.js
+import React, { useState } from "react";
+import axios from "axios";
+import "./ContactUs.css";
+
+const ContactUs = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:4000/contactUs", formData);
+      setSuccessMessage("Your message has been sent successfully!");
+      setFormData({ name: "", email: "", phone: "", message: "" });
+    } catch (error) {
+      setErrorMessage("Failed to send your message. Please try again.");
+    }
+  };
+
+  return (
+    <div className="contact-us" style={{ padding: "20px 40px" }}>
+      <h1 style={{ fontSize: "30px" }}>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Phone Number:</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message:</label>
+          <input
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="button">
+          Send Message
+        </button>
+      </form>
+      {successMessage && <p className="success-message">{successMessage}</p>}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default ContactUs;

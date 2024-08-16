fb8cbfdd085a8895ba3f74db6f1a311f15f8a709
Fatima <147131340+FatimaAnjumOfficial@users.noreply.github.com>
2024-08-16 13:26:45 +0500

    Add files via upload

---
+++ React/Travelling Website/src/data/bhotels.js
@@ -0,0 +1,120 @@
+export const bhotels = [
+  {
+    id: 1,
+    image:
+      "https://pix10.agoda.net/hotelImages/100549/0/a868182f4024c57a9b66acef1ae670dc.jpg?ca=0&ce=1&s=414x232&ar=16x9",
+    name: "Economy Lodge",
+    description:
+      "Enjoy stylish, budget-friendly accommodations in the heart of London. Experience comfort and convenience without breaking the bank. Perfect for travelers seeking quality and affordability in a vibrant city.",
+    country: "UK",
+    city: "London",
+    rooms: "364",
+    rent: "£45 - £80",
+    interior:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/68/a9/d9/indoor-pool.jpg?w=700&h=-1&s=1",
+    bedroom:
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/448059736.jpg?k=9fbd3e33be362a967e91eb2f9a63fdf7f8f4834f3ebb491403fa0fe85a3f2c4f&o=&hp=1",
+    position: [51.515, -0.1],
+  },
+  {
+    id: 2,
+    image:
+      "https://media-cdn.tripadvisor.com/media/photo-s/2c/1c/d8/a9/welcome-to-the-holiday.jpg",
+    name: "Cheap Sleep Hotel",
+    description:
+      "Stay in the vibrant city of Sydney with affordable luxury at the Cheap Sleep Hotel. Enjoy elegant amenities and a prime location at a great price.",
+    country: "Australia",
+    city: "Sydney",
+    rooms: "94",
+    rent: "$900",
+    interior:
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/125311181.jpg?k=6c5bc9dc3fdacecf31e89197cb8253efc7ea431952632629efffca36112795b7&o=&hp=1",
+    bedroom:
+      "https://www.helsinki-hotels.org/data/Photos/OriginalPhoto/14359/1435931/1435931911.JPEG",
+    position: [51.515, -0.1],
+  },
+  {
+    id: 3,
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/fe/63/dc/hotel-joy.jpg?w=1200&h=-1&s=1",
+    name: "No-Frills Hotel",
+    description:
+      "The No-Frills Hotel offers simple, yet comfortable accommodations in Berlin. Enjoy straightforward amenities and excellent value in a central location.",
+    country: "Germany",
+    city: "Berlin",
+    rooms: "73",
+    rent: "$640",
+    interior:
+      "https://sushantuniversity.edu.in/blog/wp-content/uploads/2022/05/nofrills002-1024x682.jpg",
+    bedroom:
+      "https://skift.com/wp-content/uploads/2013/01/YOTEL_NYC_PremiumCabinDusk_MK.ashx_.jpeg",
+    position: [51.505, -0.09],
+  },
+  {
+    id: 4,
+    image:
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/553899091.jpg?k=d066cef586e74bbcd6c902aadc2de16d9727fa7e5041cf76782d16705eb02aaf&o=&hp=1",
+    name: "Value Lodge",
+    description:
+      "Value Lodge in Mumbai provides a blend of historic charm and modern amenities. Relax in elegant rooms at an affordable price in a prime city location.",
+    country: "India",
+    city: "Mumbai",
+    rooms: "54",
+    rent: "$500",
+    interior:
+      "https://www.hotelsofchennai.com/data/Pics/OriginalPhoto/6966/696685/696685305/pic-marsan-lodge-chennai-10.JPEG",
+    bedroom:
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/510539939.jpg?k=1541a21662de69db61ab512921a40dc920a6bec0d0995d2811dade3f37ed9fa0&o=&hp=1",
+    position: [51.515, -0.1],
+  },
+  {
+    id: 5,
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/e4/01/5a/caption.jpg?w=700&h=-1&s=1",
+    name: "Simple Stay Inn",
+    description:
+      "Simple Stay Inn in Paris offers comfortable and budget-friendly lodging with stunning city views. Enjoy a relaxing stay with easy access to local attractions.",
+    country: "France",
+    city: "Paris",
+    rooms: "289",
+    rent: "$40,983",
+    interior:
+      "https://pix10.agoda.net/hotelImages/2186229/-1/71cb9a7b584c31b8fbb1e1cb75112110.jpg?ce=0&s=414x232&ar=16x9",
+    bedroom:
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/519220509.jpg?k=2b0e0aeabeedc96ba28523378351e1e843746de0547b9811f4d85810a8c2d5f8&o=&hp=1",
+    position: [51.505, -0.09],
+  },
+  {
+    id: 6,
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/cc/bb/33/hotel-building.jpg?w=700&h=-1&s=1",
+    name: "Smart Hotel",
+    description:
+      "The Smart Hotel in New York combines modern design with budget-conscious rates. Enjoy stylish rooms and convenient amenities in a bustling city center.",
+    country: "USA",
+    city: "New York",
+    rooms: "93",
+    rent: "$100",
+    interior:
+      "https://pix10.agoda.net/hotelImages/276868/0/c86909440acc09a73ca3fdab406c7740.jpeg?s=414x232&ar=16x9",
+    bedroom:
+      "https://media.licdn.com/dms/image/C4E12AQEY0CDiBI2-gg/article-cover_image-shrink_600_2000/0/1520236495221?e=2147483647&v=beta&t=gaR8Q5AH1-vHPHXOn4x7VzttaDBfL0tmFaYELp_Hyao",
+    position: [51.515, -0.1],
+  },
+  {
+    id: 7,
+    image: "https://thumbs.dreamstime.com/b/luxury-hotel-4480742.jpg",
+    name: "Aman Tokyo",
+    description:
+      "Discover tranquility and modern elegance at Aman Tokyo, where sleek design and serene views offer a peaceful retreat in the heart of the bustling city. ",
+    country: "Japan",
+    city: "Tokyo",
+    rooms: "304",
+    rent: "$56,973",
+    interior:
+      "https://media.architecturaldigest.com/photos/609d676464ce724873e4d1e3/master/w_1600%2Cc_limit/GettyImages-1152668.jpg",
+    bedroom:
+      "https://www.bedsland.co.uk/wp-content/uploads/2023/10/image7-64.webp",
+    position: [51.505, -0.09],
+  },
+];
